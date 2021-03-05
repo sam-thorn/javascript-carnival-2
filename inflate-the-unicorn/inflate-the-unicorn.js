b994d3bb2fc284ee754f9fe904d2064130cdde50
Sam Thorn <spthornnz@gmail.com>
2021-03-05 13:24:32 +1300

    Adds script to cycle through balloon images

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -3,3 +3,54 @@
 // -    -   -   -   -  //
 
 console.log("Inflate The Unicorn!")
+
+// var unicorn1 = [0, 1, 2];
+// var unicorn2 = [];
+
+
+// document.getElementById("image-1").onclick = function unicornClick() {
+//     document.getElementById("image-1").src = "./images/unicorn-1.png";
+// }
+
+document.getElementById("image-1").onclick = function unicornClick() {
+  var imageChange = document.getElementById("image-1");
+
+  if (imageChange.src.match("./images/unicorn-0.png")) {
+    imageChange.src = "./images/unicorn-1.png"; 
+  } else if (imageChange.src.match("./images/unicorn-1.png")) {
+    imageChange.src = "./images/unicorn-2.png"; 
+  } else if (imageChange.src.match("./images/unicorn-2.png")) {
+    imageChange.src = "./images/unicorn-3.png";
+  } else {
+    alert('Unicorn Number 1 Says Thank You!');
+  }
+}
+
+document.getElementById("image-2").onclick = function unicornClick() {
+  var imageChange = document.getElementById("image-2");
+
+  if (imageChange.src.match("./images/unicorn-0.png")) {
+    imageChange.src = "./images/unicorn-1.png"; 
+  } else if (imageChange.src.match("./images/unicorn-1.png")) {
+    imageChange.src = "./images/unicorn-2.png"; 
+  } else if (imageChange.src.match("./images/unicorn-2.png")) {
+    imageChange.src = "./images/unicorn-3.png";
+  } else {
+    alert('Unicorn Number 2 Says Thank You!');
+  }
+}
+
+document.getElementById("image-3").onclick = function unicornClick() {
+  var imageChange = document.getElementById("image-3");
+
+  if (imageChange.src.match("./images/unicorn-0.png")) {
+    imageChange.src = "./images/unicorn-1.png"; 
+  } else if (imageChange.src.match("./images/unicorn-1.png")) {
+    imageChange.src = "./images/unicorn-2.png"; 
+  } else if (imageChange.src.match("./images/unicorn-2.png")) {
+    imageChange.src = "./images/unicorn-3.png";
+  } else {
+    alert('Unicorn Number 3 Says Thank You!');
+  }
+}
+
